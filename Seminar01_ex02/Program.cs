3bb6341251c64996e7dde459751d5b455b13ad56
Inna Belkina <inna0311@inbox.ru>
2023-01-22 20:55:22 +0600

    Найти квадрат числа

---
+++ Seminar01_ex02/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт его квадрат.
+
+// 4 -> 16
+// -3 -> 9
+
+Console.WriteLine("Введите число:");
+// string stringNumber = Console.ReadLine();
+// int number;
+// while(!int.TryParse(stringNumber, out number))
+// {
+//     System.Console.WriteLine("Ошибка! Введите целое число");
+//     number = Console.ReadLine().Trim();
+// }
+int number = Convert.ToInt32(Console.ReadLine());
+int result = number * number;
+
+Console.WriteLine("Квадрат числа " + number + " будет равен " + result);

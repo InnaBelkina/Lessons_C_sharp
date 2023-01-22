2361a380c415e325b1a32bd1ff97bacd9a7163f7
Inna Belkina <inna0311@inbox.ru>
2023-01-22 20:54:21 +0600

    Задача 2, Дз1

---
+++ Homework01_Task02/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+
+Console.WriteLine("Введите число №1");
+int number_1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число №2");
+int number_2 = Convert.ToInt32(Console.ReadLine());
+
+if (number_1 == number_2)
+{
+    Console.WriteLine("Числа равны");
+}
+else
+{
+
+    if (number_1 > number_2)
+    {
+        Console.WriteLine("Число " + number_1 + " больше числа " + number_2);
+    }
+    else
+    {
+        Console.WriteLine("Число " + number_2 + " больше числа " + number_1);
+    }
+}
+
+// Тут можно обойтись без вложенных блоков, если использовать конструкцию if – else if – else:
+
+// if (num1 > num2)
+// …
+// else if (num1 < num2)
+// …
+// else
+// ...

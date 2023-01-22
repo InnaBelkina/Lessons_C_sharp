f42aa7e7be44ae7b9c61b421ea281ffc85d11c79
Inna Belkina <inna0311@inbox.ru>
2023-01-22 20:54:51 +0600

    Пример с именем

---
+++ Seminar01_ex01/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+Console.WriteLine("Введите Ваше имя");
+string username = Console.ReadLine();
+
+if (username == "Илья")
+{
+    Console.WriteLine("Ура! Привет, Илья!");
+}
+else
+{
+    Console.WriteLine($"Привет, {username}!");
+}

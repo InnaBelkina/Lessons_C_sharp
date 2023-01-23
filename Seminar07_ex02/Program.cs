432ed62e7fdd0c8b179dd2e2a2e7d965a5bf9b05
Inna Belkina <inna0311@inbox.ru>
2023-01-23 16:17:22 +0600

    Задать массив по формуле м+н

---
+++ Seminar07_ex02/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задача 48: Задайте двумерный массив размера m на n, каждый элемент 
+// в массиве находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+// int[,] CreateArray(int rows, int columns, int minValue, int maxValue)
+//     {
+//         int[,] array = new int[rows, columns];
+//         Random random = new Random();
+//         foreach (int item in array)
+//         {
+//             item = random.Next(minValue, maxValue + 1);
+//         }
+//         return array;
+//     }
+
+
+// Задача 48: Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+int[,] CreateArray(int row, int col)
+{
+    int[,] Array = new int[row, col];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            Array[i, j] = i + j;
+        }
+        }
+        return Array;
+}
+int[,] array = CreateArray(3,4);
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+PrintArray(array);

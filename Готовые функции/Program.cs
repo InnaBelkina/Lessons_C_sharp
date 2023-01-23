dc0249992761604b0bdea40ae84499fedb123ba3
Inna Belkina <inna0311@inbox.ru>
2023-01-23 16:21:41 +0600

    Готовые функции

---
+++ Готовые функции/Program.cs
@@ -0,0 +1,29 @@
+﻿//  Задать рандомный двумерный массив
+
+ static int[,] CreateRandomArray(int rows, int columns, int minValue, int maxValue)
+    {
+        int[,] array = new int[rows, columns];
+        Random random = new Random();
+        for (int i = 0; i < array.GetLength(0); i++)
+            for (int j = 0; j < array.GetLength(1); j++)
+                array[i, j] = random.Next(minValue, maxValue + 1);
+        return array;
+    }
+    // затем например: 
+    // int[,] array = CreateRandomArray(5, 5, 0, 10);
+
+
+//  Вывести двумерный массив
+
+static void PrintArray(int[,] array)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+                Console.Write($"{array[i, j]} ");
+            Console.WriteLine();
+        }
+    }
+
+// Затем
+//   PrintArray(array);

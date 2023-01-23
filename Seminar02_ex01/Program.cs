8ec3a01ae292d6c5460c24838b75ec3fe5087a30
Inna Belkina <inna0311@inbox.ru>
2023-01-23 13:42:03 +0600

    БОльшая цифра в двухзначном числе

---
+++ Seminar02_ex01/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10,99] и показывает наибольшую цифру  числа.
+
+// 78 -> 8
+// 56 -> 6
+
+int number = new Random().Next(10,100);
+Console.WriteLine(number);
+// if ( number / 10 > number % 10 ) Console.WriteLine(number/10);
+// else Console.WriteLine(number % 10);
+
+// Console.WriteLine (number / 10 > number % 10 ? number / 10 : number % 10 );
+
+ Console.WriteLine (number / 10 > number % 10 ? 
+ number / 10 : 
+ number % 10 );
+
+//  string stringNumber = number.ToString();
+//  stringNumber[0]
+//  stringNumber[1]

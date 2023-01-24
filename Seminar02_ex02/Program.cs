d16f5d5c311f5e8b8b119313c030780113908961
Inna Belkina <inna0311@inbox.ru>
2023-01-24 11:43:26 +0600

    В трехзначном числе убираем вторую цифру

---
+++ Seminar02_ex02/Program.cs
@@ -0,0 +1,10 @@
+﻿//  Принимаем трехзначное число и убираем из него вторую цифру
+//  456 -> 46
+
+// Console.Write("Введите трехзначное число: ");
+// string stringNumber = Console.ReadLine().Trim(); // trim убирает пробел с начала строки и с конца строки
+// System.Console.WriteLine($"{stringNumber} -> {stringNumber[0]}{stringNumber[2]}");
+
+int number = new Random().Next(10,1000);
+Console.WriteLine ($"Рандомное число: {number}");
+Console.WriteLine ($"Ответ {number / 100 * 10 + number % 10}");

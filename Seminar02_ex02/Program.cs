6a85fc6bf72e0fc750053d5f7145307f137b5806
Inna Belkina <inna0311@inbox.ru>
2023-01-24 13:12:03 +0600

    Из 3-х знач числа убрать вторую цифру

--- Seminar02_ex02/Program.cs
+++ Seminar02_ex02/Program.cs
@@ -5,6 +5,20 @@
 // string stringNumber = Console.ReadLine().Trim(); // trim убирает пробел с начала строки и с конца строки
 // System.Console.WriteLine($"{stringNumber} -> {stringNumber[0]}{stringNumber[2]}");
 
-int number = new Random().Next(10,1000);
-Console.WriteLine ($"Рандомное число: {number}");
-Console.WriteLine ($"Ответ {number / 100 * 10 + number % 10}");
+
+// int number = new Random().Next(10,1000);
+// Console.WriteLine ($"Рандомное число: {number}");
+// Console.WriteLine ($"Ответ {number / 100 * 10 + number % 10}");
+
+
+// int number = new Random().Next(100,1000);
+// Console.WriteLine(number);
+
+string str = number.ToString();
+int[] array = new int[str.Length];
+for (int i = 0; i < str.Length; i++)
+{
+    array[i] = int.Parse(str[i].ToString());
+}
+Console.Write(array[0]);
+Console.WriteLine(array[2]);

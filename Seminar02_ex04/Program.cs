53cd5bf881845019a4e54db37ac2ebe894f90c9d
Inna Belkina <inna0311@inbox.ru>
2023-01-24 12:22:20 +0600

    проверка кратности числа на 7 и 23 одновременно

---
+++ Seminar02_ex04/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно 7 и 23 одновремненно
+
+int number = new Random().Next(0, 1000);
+System.Console.WriteLine($"Рандомное число равно {number}");
+
+if (number % 7 == 0 && number % 23 == 0) System.Console.WriteLine($"{number} -> да");
+else System.Console.WriteLine($"{number} -> нет");

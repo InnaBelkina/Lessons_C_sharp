7d593331d4ab46c7e9105d869e4577d364cc8d1a
Inna Belkina <inna0311@inbox.ru>
2023-01-25 14:07:48 +0600

    Задача с добавлением в массив чисел с консоли

---
+++ Homework/Homework06/Homework06_Task41/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Сколько чисел Вы хотите ввести?");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[size];
+
+for (int i = 0; i < size; i++)
+{
+    Console.Write("Введите целое число: ");
+    array[i] = int.Parse(Console.ReadLine());
+}
+
+//  Console.WriteLine(string.Join("," , array));
+
+int posNum = 0;
+for (int i = 0; i < size; i++)
+{
+    if (array[i] > 0) posNum++;
+}
+
+Console.WriteLine($"Количество чисел больше 0 равно {posNum}");

df78f23748da97e9e5683312de8dcd6fb12f31fc
Inna Belkina <inna0311@inbox.ru>
2023-01-25 15:02:55 +0600

    Сколько чисел >0 ввел пользователь

--- Homework/Homework06/Homework06_Task41/Program.cs
+++ Homework/Homework06/Homework06_Task41/Program.cs
@@ -23,3 +23,19 @@ for (int i = 0; i < size; i++)
 }
 
 Console.WriteLine($"Количество чисел больше 0 равно {posNum}");
+
+
+
+// Console.WriteLine("Введите несколько чисел: ");
+// int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+// int count = 0;
+
+// for (int i = 0; i < arr.Length; i++)
+// {
+//     if (arr[i] > 0)
+//     {
+//         count++;
+//     }
+// }
+
+// Console.WriteLine($"Кол-во элементов > 0: {count}");

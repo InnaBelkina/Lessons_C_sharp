9ad8c5725ab0b4a8a0496b0d3517b169d41b5541
Inna Belkina <inna0311@inbox.ru>
2023-01-28 16:41:28 +0600

    Написали методы для заполнения массива числами и вывод массива

---
+++ Lection04_ex01/Program.cs
@@ -0,0 +1,61 @@
+﻿string[,] table = new string[2, 5];
+// table [0,0] table [0,1]  table [0,2] ...
+// table [1,0]
+// table[1,2] = "слово";
+
+// for (int rows = 0; rows < 2; rows++)
+// {
+//     for (int columns = 0; columns < 5; columns++)
+//     {
+//         Console.WriteLine($"{table[rows, columns]}");
+//     }
+// }
+
+
+
+// int[,] matrix = new int[3, 4];
+
+// for (int i = 0; i < matrix.GetLength(0); i++)     // matrix.GetLength(0) - количество строк
+// {
+//     for (int j = 0; j < matrix.GetLength(1); j++)
+//     {
+//         Console.Write($"{matrix[i, j]}");
+//     }
+//      Console.WriteLine();         // Чтобы был переход на новую строку.
+// }
+
+
+// Пишем метод для построение двумерного массива
+
+void PrintArray(int [,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]}");
+        }
+        Console.WriteLine();
+    }
+}
+
+// Метод, чтобы заполнять массив случайными числами
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(1,10);
+        }
+    }
+}
+
+
+int[,] matrix = new int[3, 4];
+PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
+

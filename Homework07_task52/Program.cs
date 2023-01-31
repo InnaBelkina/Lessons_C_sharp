7f2f49509782dd5f7e07f331829bf8f1491a17c6
Inna Belkina <inna0311@inbox.ru>
2023-01-31 19:26:55 +0600

    Ср Ариф каждого столбца в двумерном массиве

---
+++ Homework07_task52/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+Console.Clear();
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+Console.WriteLine("Задайте количество строк двумерного массива:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте количество столбцов двумерного массива:");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[m, n];
+FillArray(matrix);
+PrintArray(matrix);
+
+double srAr = 0;
+double sum = 0;
+for (int k = 0; k < n; k++)
+{
+    for (int t = 0; t < m; t++)
+    {
+        sum = sum + matrix[t, k];
+        // Console.WriteLine(sum);
+    }
+    srAr = sum / m;
+    Console.Write($"{Math.Round(srAr, 2)};  ");
+    sum = 0;
+}
+
+
+

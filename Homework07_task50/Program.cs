578c797bceb704e7c43d6d3bb5c587aa346d60f5
Inna Belkina <inna0311@inbox.ru>
2023-01-31 17:15:19 +0600

    Вывод элемента по порядковому номеру

---
+++ Homework07_task50/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+// 17 -> такого числа в массиве нет
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+Console.WriteLine("Задайте количество строк двумерного массива:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте количество столбцов двумерного массива:");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[m, n];
+FillArray(matrix);
+PrintArray(matrix);
+
+Console.WriteLine("Введите номер элемента в массиве:");
+int position = Convert.ToInt32(Console.ReadLine());
+int k = 0;
+
+if (position > (m * n)) Console.WriteLine("Такого числа нет");
+else
+{
+    while (position > n)
+    {
+        position = position - n;
+        k++;
+    }
+    object c = matrix.GetValue(k, position - 1);
+    Console.WriteLine(c);
+}
+
